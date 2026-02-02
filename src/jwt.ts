426bc216fe69c6d75867eaaf0bdf0b45c114c7f9
KTibow <KTibow@users.noreply.github.com>
2026-02-01 16:03:17 -0800

    make session not ephemeral

--- src/jwt.ts
+++ src/jwt.ts
@@ -2,6 +2,7 @@ import { env } from "cloudflare:workers";
 import { sign, verify } from "@tsndr/cloudflare-worker-jwt";
 import { RequestCookieStore } from "@worker-tools/request-cookie-store";
 
+const maxAge = 30 * 24 * 60 * 60 * 1000;
 export const getAuth = async (cookieStore: RequestCookieStore) => {
   const jwtInfo = await cookieStore.get("jwt");
   if (!jwtInfo) throw new Response("Unauthorized", { status: 401 });
@@ -18,7 +19,7 @@ export const getAuth = async (cookieStore: RequestCookieStore) => {
 };
 export const setAuth = async (cookieStore: RequestCookieStore, sub: string) => {
   const jwt = await sign({ sub }, env.JWT_SECRET);
-  await cookieStore.set({ name: "jwt", value: jwt, httpOnly: true });
+  await cookieStore.set({ name: "jwt", value: jwt, httpOnly: true, expires: Date.now() + maxAge });
 };
 export const deleteAuth = async (cookieStore: RequestCookieStore) => {
   await cookieStore.delete("jwt");
